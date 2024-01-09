42613749fe0e30f7a22d757fe83bb51b9d0f774d
Webgoonie <webgoonie@gmail.com>
2024-01-08 21:53:01 -0500

    Added index.php to africars.io

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Afracars.io Offical Home Page</title>
+</head>
+
+<body>
+	<div id="page_title" class="page_title" style="text-align: center;">
+		<h2>africars.io</h2>
+        <p>It's Here To Stay</p>
+        <p>Find your next vehicle in Africa on Africars.io</p>
+    </div>
+</body>
+</html>
