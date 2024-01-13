3cfb9ef0d63f0f4468515e346fd8855e4ce0474d
Webgoonie <webgoonie@gmail.com>
2024-01-13 05:03:10 -0500

    Testing Contact Us Page

---
+++ nextclient/src/app/contactus/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ContactUs = () => {
+    return (
+        <div>
+            <h1>Contact Us Page</h1>
+            <p>This is the Contact us Page you are seeing...</p>
+
+        </div>
+    )
+}
+
+export default ContactUs
