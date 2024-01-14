699c6150e96734f29296369d4b66ea1e142d62db
Webgoonie <webgoonie@gmail.com>
2024-01-13 19:46:26 -0500

    Designing Working Layout

---
+++ src/app/about/about.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+const about = () => {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div>
+            About Us Page Homie
+            </div>
+        </main>
+    )
+}
+
+export default about
