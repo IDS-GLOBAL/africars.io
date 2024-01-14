aeac5a01bbbfcc027b65792107de1a80805204bb
Webgoonie <webgoonie@gmail.com>
2024-01-14 04:02:18 -0500

    Fetching route using client from home but no data from prisma

---
+++ src/app/api/getPosts/route.ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "../../../../prisma/client";
+
+type ResponseData = {
+    message: string
+}
+
+console.log(' Hit My man route.tsx')
+
+//async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextRequest, res: NextResponse) {
+
+    console.log('Line 14 getPosts Hit req', req)
+    console.log('Line 15 getPosts Hit res', res)
+    //res.status(200).json({ name: 'John Doe' })
+    if(req.method === 'GET'){
+        try {
+            //Get primsa to fetch the post
+            const data = prisma.post.findMany();
+            console.log('Line 21 data: ' + JSON.stringify(data))
+            return NextResponse.json(data);
+        } catch (error) {
+            return NextResponse.json(error);
+        }
+    }
+
+}
+
+export { handler as GET, handler as POST }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,38 @@
+'use client'
+
 import React from 'react'
 import Link from 'next/link'
 
-const Home = () => {
-  return (
+async function getPosts(){
+  //const res = await fetch(`${process.env.BASE_URL}`+'/api/getPosts')
+  //const res = await fetch(`${process.env.BASE_URL}api/getPosts`, { cache: 'force-cache' })
+  //const res = await fetch(`http://localhost:5556/api/getPosts`, { cache: 'force-cache' })
+  const res = await fetch(`http://localhost:5556/api/getPosts`)
+  //const res = await fetch(`/api/getPosts`)
+  console.log('Line 9 res: ', res)
+
+  if(!res.ok){
+    console.log('res Not Okay res: ', res)
+    return  new Error("failed to fetch");
+    
+  }
+}
+
+const Home = async () => {
+
+  const data = await getPosts()
+  
+  console.log('Home Line 22 data: ', data)
+  return ( 
     <main className="py-8 px-48">
       <div>
-          <p className='p-2'><Link className='bg-teal-700 text-black font-medium p-2' href="{/}">Home Page</Link></p>
-          <p className='p-2'><Link className='bg-teal-700 text-black font-medium p-2' href="{/dashboard}">Dashboard</Link></p>
-          <p className='p-2'><Link className='bg-teal-700 text-black font-medium p-2' href="{/about}">About us</Link></p>
+          <p className='p-2'><Link className='bg-teal-700 text-black font-medium p-2' href="{./}">Home Page</Link></p>
+          <p className='p-2'><Link className='bg-teal-700 text-black font-medium p-2' href="{./dashboard}">Dashboard</Link></p>
+          <p className='p-2'><Link className='bg-teal-700 text-black font-medium p-2' href="{./about}">About us</Link></p>
+      </div>
+
+      <div>
+
       </div>
     </main>
   )
